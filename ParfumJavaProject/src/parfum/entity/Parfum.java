8d1643e4bee1a23eace7e855329251aec17d74aa
IS96811 <IS96811@T2LPRVT2146.gm.isbank.com.tr>
2017-07-30 16:22:23 +0300

    parfum2

--- ParfumJavaProject/src/parfum/entity/Parfum.java
+++ ParfumJavaProject/src/parfum/entity/Parfum.java
@@ -2,6 +2,7 @@ package parfum.entity;
 
 public class Parfum {
 	private String name;
+	private String brand;
 
 	public String getName() {
 		return name;
@@ -9,5 +10,13 @@ public class Parfum {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
 	}
 }
