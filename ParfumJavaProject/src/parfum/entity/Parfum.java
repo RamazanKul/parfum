f9c87de012790b8502c690bdbb6bfd6b8abc8bc5
IS96811 <IS96811@T2LPRVT2146.gm.isbank.com.tr>
2017-07-30 15:53:11 +0300

    parfüm entity oluştu

---
+++ ParfumJavaProject/src/parfum/entity/Parfum.java
@@ -0,0 +1,13 @@
+package parfum.entity;
+
+public class Parfum {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
